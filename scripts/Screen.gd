6ba9d20d2d07d1bb9ea26e892914ac1df8bec06b
ryschneider <34727856+ryschneider@users.noreply.github.com>
2023-07-15 23:47:27 +1000

    fsaf

--- scripts/Screen.gd
+++ scripts/Screen.gd
@@ -22,12 +22,3 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("switch_dimension"):
 		switch()
-#	cameraController(0)
-
-#func cameraController(followLineX):
-#	if $Camera2D.position.x > followLineX && staticCamera == false:
-#
-#	else:
-#		$Camera2D.reparent(screenNode,false)
-#		$Camera2D.position = Vector2(0, 0)
-#
