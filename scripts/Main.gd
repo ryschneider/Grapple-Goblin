ff04c8033f2ca11e2e6873d4e5b3b085ed65a5f9
lachl-coup <139621575+lachl-coup@users.noreply.github.com>
2023-07-15 23:42:51 +1000

    haiosehjd
    asdada

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,14 +1,6 @@
 extends Node2D
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-const screens = ["res://screens/Screen1.tscn", "res://screens/Screen2.tscn", "res://screens/Screen3.tscn", "res://screens/Screen2.tscn", "res://screens/Screen5.tscn", "res://screens/Screen7.tscn"]
-=======
-const screens = ["res://screens/Screen1.tscn", "res://screens/Screen2.tscn", "res://screens/Screen5.tscn"]
->>>>>>> Stashed changes
-=======
-const screens = ["res://screens/Screen1.tscn", "res://screens/Screen2.tscn", "res://screens/Screen3.tscn", "res://screens/Screen4.tscn", "res://screens/Screen5.tscn", "res://screens/Screen6.tscn", "res://screens/Screen7.tscn"]
->>>>>>> d783519a93f63369a9c7f84646f018c0f6d365eb
+const screens = ["res://screens/Screen1.tscn", "res://screens/Screen2.tscn", "res://screens/Screen3.tscn", "res://screens/Screen2.tscn", "res://screens/Screen5.tscn", "res://screens/Screen6.tscn", "res://screens/Screen11.tscn"]
 
 var currentScreen
 
@@ -23,15 +15,7 @@ func loadScreen(id):
 	add_child(screenLoad.instantiate())
 
 func _ready():
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-	loadScreen(4)
-=======
-	loadScreen(2)
->>>>>>> Stashed changes
-=======
-	loadScreen(5)
->>>>>>> d783519a93f63369a9c7f84646f018c0f6d365eb
+	loadScreen(6)
 
 func _process(delta):
 	pass
