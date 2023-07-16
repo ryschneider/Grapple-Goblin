70f163f8ad672e6471404530661b4cbeec85af14
ryschneider <34727856+ryschneider@users.noreply.github.com>
2023-07-16 13:52:32 +1000

    hook sfx

--- scripts/SFX.gd
+++ scripts/SFX.gd
@@ -3,6 +3,7 @@ extends AudioStreamPlayer2D
 var jump = preload("res://assets/SFX/jump.wav")
 var button = preload("res://assets/SFX/button.wav")
 var pickup = preload("res://assets/SFX/PickupItem.wav")
+var hook = preload("res://assets/SFX/hook.wav")
 
 func _ready():
 	pass
@@ -24,3 +25,8 @@ func playPickup():
 
 func inPickup():
 	return stream == pickup and get_playback_position() < 1.2
+
+func playHook():
+	volume_db = -5
+	stream = hook
+	play()
