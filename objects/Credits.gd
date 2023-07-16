8a45b1b3d0ccc71484f8dd855eac94f51cc281a8
ryschneider <34727856+ryschneider@users.noreply.github.com>
2023-07-16 15:20:12 +1000

    credits bg

--- objects/Credits.gd
+++ objects/Credits.gd
@@ -3,20 +3,26 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Bg.show()
 	$First.hide()
 	$Second.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-var time = 0
-const STAGE_2 = 5
+var time = -10
+const STAGE_2 = 2
 const PROD_DELAY = 2
-const STAGE_3 = 10
-const STAGE_4 = 15
-const STAGE_5 = 20
+const STAGE_3 = STAGE_2 + 5
+const STAGE_4 = STAGE_3 + 5
+const STAGE_5 = STAGE_4 + 5
 func _process(delta):
 	time += delta
 	
-	if time < STAGE_2:
+	if time < 0:
+		$Bg.show()
+		$First.hide()
+		$Second.hide()
+	elif time < STAGE_2:
+		$Bg.hide()
 		$First.hide()
 		$Second.hide()
 	elif time < STAGE_3:
@@ -25,7 +31,6 @@ func _process(delta):
 		
 		$First/production.show()
 		$First/production.modulate[3] = (time - STAGE_2 - PROD_DELAY) / (STAGE_3-STAGE_2 - PROD_DELAY)
-		print($First/production.modulate)
 		
 		$First/names.hide()
 	elif time < STAGE_4:
@@ -36,4 +41,4 @@ func _process(delta):
 	else:
 		FileAccess.open("user://beatgame.txt", FileAccess.WRITE).store_string("yes")
 		FileAccess.open("user://showlevels.txt", FileAccess.WRITE).store_string("yes")
-		get_tree().change_scene_to_file("res://objects/Main.tscn")
+		get_tree().change_scene_to_file("res://objects/Menu.tscn")
